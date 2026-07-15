11745dfe47b48179f5cb244e2a7c60e336eb87eb
Dan Barua <danbarua@gmail.com>
2026-07-15 10:41:31 +0100

    Deprecate cognitive-kernel C code

--- cognitive-kernel/run.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-echo "GCC:"
-./bin/phase_kernel_gcc
-#echo "\n\nClang:"
-#./bin/phase_kernel_clang
--- cognitive-kernel/run_avx.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-echo "GCC:"
-./bin/avx_shift_gcc
-echo "Clang:"
-./bin/avx_shift_clang
